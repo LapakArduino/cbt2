90f55efe0d2e6b167bc17b3f64212484582a297b
LapakArduino <90664098+LapakArduino@users.noreply.github.com>
2023-03-24 13:23:22 +0700

    revisi lagi kang 5.2

--- application/config/database.php
+++ application/config/database.php
@@ -76,9 +76,9 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'zyacbt',
+	'username' => 'kangekom_eko',
+	'password' => '=]g)ioY5H9ws',
+	'database' => 'kangekom_laravel',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
