1295a0e6f05e122ea892a9a6c5419288b44cbdfa
LapakArduino <90664098+LapakArduino@users.noreply.github.com>
2023-03-30 11:27:39 +0700

    add some to print test card 2.2

--- application/config/database.php
+++ application/config/database.php
@@ -81,7 +81,7 @@ $db['default'] = array(
 	// 'password' => '',
 	// 'database' => 'zyacbt',
 
-	'hostname' => 'localhost',
+	'hostname' => 'localhost3',
 	'username' => 'kangekom_eko',
 	'password' => '=]g)ioY5H9ws',
 	'database' => 'kangekom_laravel',
